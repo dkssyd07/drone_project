e29a8da1d4ba1fad1eb2f6ce7da6a38ccd7c946c
Dongho Kim <line102784@gmail.com>
2025-07-01 00:22:42 -0600

    Update - LED / Measured Distance

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,45 @@
 #include <Arduino.h>
 
-#define TRIG_PIN 5
-#define ECHO_PIN 18
+#define TRIG_PIN 5      
+#define ECHO_PIN 18      
+#define LED_PIN 2        
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-}
 
-void loop() {
+float getDistanceCM() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-  long duration = pulseIn(ECHO_PIN, HIGH);
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);  
   float distance = duration * 0.034 / 2;
+  return distance;
+}
 
-  Serial.print("Distance: ");
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+}
+
+void loop() {
+  float distance = getDistanceCM();
+  Serial.print("Measured Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  delay(500);
+  if (distance > 0 && distance < 30) {
+   
+    digitalWrite(LED_PIN, HIGH);
+    delay(200);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+  } else {
+    
+    digitalWrite(LED_PIN, LOW);
+    delay(400);
+  }
 }
